4d695708dbbdab9241b6b4d712a66f1519285541
Blind Pew <49527328+KonstantinOsipov@users.noreply.github.com>
2024-01-06 10:59:46 +0700

    1-й Урок.

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+client = app.test_client()
+
+tutorials = [
+    {
+        'title': 'Video #1. Intro',
+        'description': 'GET, POST routes'
+    },
+    {
+        'title': 'Video #2. More features',
+        'description': 'PUT, DELETE routes'
+    }
+]
+
+@app.route('/tutorials', methods=['GET'])
+def get_list():
+    return jsonify(tutorials)
+
+@app.route('/tutorials', methods=['POST'])
+def update_list():
+    new_one = request.json
+    tutorials.append(new_one)
+    return jsonify(tutorials)
+
+if __name__ == '__main__':
+    app.run()
